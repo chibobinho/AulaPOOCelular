5386fe2c80e04f4d55d67f626f1a2c89051c0ec0
Chibo <yurizin163@gmail.com>
2021-05-19 11:34:34 -0300

    Exercicio finalizado

---
+++ Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+
+namespace aulaPOOCelular
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            celular xiaomi = new celular();
+
+            Console.WriteLine("Quer ligar o celular? (S/N)");
+
+            string desejo = Console.ReadLine().ToLower();
+            while (xiaomi.ligado == false)
+            {
+
+                if (desejo == "s")
+                {
+                    Console.WriteLine($@"
+__________________________
+|           o            |
+|                        |
+|    1 - Fazer ligação   |
+|                        |
+|    2 - Mandar          |
+|       mensagem         |
+|                        |
+|    3 - Propriedades    |
+|                        |
+|    0 - Desligar        |
+|                        |
+==========================
+            ");
+                }
+                else if (desejo == "n")
+                {
+                    xiaomi.ligado = true;
+                    Console.WriteLine("Quer ir embora mesmo? Digite 0");
+                }
+
+                int opcao = int.Parse(Console.ReadLine());
+                if (opcao == 1)
+                {
+                    xiaomi.ligado = false;
+                    Console.WriteLine($@"
+__________________________
+|           o            |
+|                        |
+|                        |
+|                        |
+|       Ligação não      |
+|        atendida        |
+|           :(           |
+|                        |
+|                        |
+|                        |
+|                        |
+==========================
+            ");
+                }
+                else if (opcao == 2)
+                {
+                    xiaomi.ligado = false;
+                    Console.WriteLine($@"
+__________________________
+|           o            |
+|                        |
+|                        |
+|                        |
+|      Mensagem foi      |
+|        enviada!        |
+|                        |
+|                        |
+|                        |
+|                        |
+|                        |
+==========================
+            ");
+                }
+                else if (opcao == 3)
+                {
+                    xiaomi.ligado = false;
+                    Console.WriteLine($@"
+__________________________
+|           o            |
+|                        |
+|                        |
+| modelo {xiaomi.modelo} |
+| cor {xiaomi.cor}             |
+| tamanho {xiaomi.tamanho}              |
+|                        |
+|                        |
+|                        |
+|                        |
+|                        |
+==========================
+            ");
+                }
+                else if (opcao == 0)
+                {
+                    xiaomi.ligado = true;
+                    Console.WriteLine($@"
+__________________________
+|           o            |
+|                        |
+|                        |
+|                        |
+|                        |
+|       desligado        |
+|          :(            |
+|                        |
+|                        |
+|                        |
+|                        |
+==========================
+            ");
+                }
+            }
+        }
+    }
+}
+
+
---
+++ celular.cs
@@ -0,0 +1,10 @@
+namespace aulaPOOCelular
+{
+    public class celular
+    {
+        public string cor = "Branco";
+        public string modelo = "Xiaomi redmi 9s";
+        public int tamanho = 7;
+        public bool ligado = false;
+    }
+}
